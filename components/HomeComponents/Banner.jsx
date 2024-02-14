a58417f6955d6ac32f90938b029ef7412d9578d4
hongsungin92 <hongsungin92@gmail.com>
2024-02-15 01:22:52 +0900

    ..

--- components/HomeComponents/Banner.jsx
+++ components/HomeComponents/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
         className="absolute inset-0 z-20 flex flex-col items-center py-6 justify-center w-full h-full"
         //bg-gradient-to-t from-MidNightBlack
       >
-        <div className="bg-LightGray/10 w-[95%] h-[90%] px-4 py-2 rounded-xl overflow-hidden flex md:block">
+        <div className="bg-LightGray/10 w-[95%] h-[90%] px-4 py-2 rounded-xl overflow-hidden mt-[30px]">
           <div className="flex items-center md:items-center md:justify-around">
             <div className="flex flex-col gap-[8px] w-full md:items-center">
               <div className="">
