401a10bd1637556d71fbbedb4ef1006a1d38b473
hongsungin92 <hongsungin92@gmail.com>
2024-03-06 23:41:10 +0900

    ..

--- components/templates/Home/Home.tsx
+++ components/templates/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Banner from "../../HomeComponents/Banner";
 import { CommercialProjects, Layout } from "../../organisms";
 import { Sidebar } from "@/components/organisms";
@@ -8,42 +9,71 @@ import About from "./About";
 const cx = classNames.bind(styles);
 
 const Home = () => {
+  const [nav, setNav] = useState<string>("about");
   return (
     <main className={cx("home")}>
       <Sidebar />
       <div className={cx("main-content")}>
         <nav className={cx("navbar")}>
-          <ul className="navbar-list">
+          <ul className={cx("navbar-list")}>
             <li className="navbar-item">
-              <button className="navbar-link active" data-nav-link>
+              <button
+                className={cx("navbar-link", {
+                  active: nav === "about",
+                })}
+                data-nav-link
+                onClick={() => setNav("about")}
+              >
                 About
               </button>
             </li>
             <li className="navbar-item">
-              <button className="navbar-link" data-nav-link>
+              <button
+                className={cx("navbar-link", {
+                  active: nav === "resume",
+                })}
+                data-nav-link
+                onClick={() => setNav("resume")}
+              >
                 Resume
               </button>
             </li>
             <li className="navbar-item">
-              <button className="navbar-link" data-nav-link>
+              <button
+                className={cx("navbar-link", {
+                  active: nav === "portfolio",
+                })}
+                data-nav-link
+                onClick={() => setNav("portfolio")}
+              >
                 Portfolio
               </button>
             </li>
             <li className="navbar-item">
-              <button className="navbar-link" data-nav-link>
+              <button
+                className={cx("navbar-link", {
+                  active: nav === "blog",
+                })}
+                data-nav-link
+                onClick={() => setNav("blog")}
+              >
                 Blog
               </button>
             </li>
             <li className="navbar-item">
-              <button className="navbar-link" data-nav-link>
+              <button
+                className={cx("navbar-link", {
+                  active: nav === "contact",
+                })}
+                data-nav-link
+                onClick={() => setNav("contact")}
+              >
                 Contact
               </button>
             </li>
           </ul>
         </nav>
-        <article className={cx("article")}>
-          <About />
-        </article>
+        <article className={cx("article")}>{nav === "about" && <About />}</article>
       </div>
     </main>
   );
