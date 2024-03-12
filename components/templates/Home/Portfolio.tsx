58edd63268985fbdf9c2c60eca3b057efa8d4927
hongsungin92 <hongsungin92@gmail.com>
2024-03-13 01:36:39 +0900

    ..

--- components/templates/Home/Portfolio.tsx
+++ components/templates/Home/Portfolio.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Image from "next/image";
 import classNames from "classnames/bind";
 import styles from "./index.module.scss";
 
@@ -58,6 +59,25 @@ const Portfolio = ({ className }: PortfolioProps) => {
             >
               Mobile Apps
             </button>
+          </li>
+        </ul>
+        <ul className={`${cx("project-list")}`}>
+          <li className={`${cx("project-item")}`} data-filter-item data-category="web development">
+            <a href="#">
+              <figure className={`${cx("project-img")}`}>
+                {/* <div className="project-item-icon-box">
+                  <ion-icon name="eye-outline"></ion-icon>
+                </div> */}
+
+                <Image src="/images/gconstudio.png" alt="gconstudio" loading="lazy" width={2000} height={1247} />
+              </figure>
+
+              <h3 className="project-title">지콘스튜디오</h3>
+              <h3 className="project-title">Twigfarm</h3>
+              <h3 className="project-title">ReactJS MongoDB AWS Material-UI MeteorJS</h3>
+
+              <p className="project-category">Web development</p>
+            </a>
           </li>
         </ul>
       </section>
